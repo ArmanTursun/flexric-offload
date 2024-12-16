7a45201991626dcf1fe8e6f641582575c0996e1f
ArmanTursun <armantursun94@gmail.com>
2024-12-16 19:02:54 +0900

    add resutls figs

--- examples/xApp/python3/xapp_drl_ra.py
+++ examples/xApp/python3/xapp_drl_ra.py
@@ -64,7 +64,6 @@ class VITS(object):
         self.nb_updates = nb_updates
         self.mean_prior = mean_prior
         self.mean = torch.tensor(mean_prior, dtype=torch.float32).to(self.device)
-        #self.mean = mean_prior.clone().detach().to(self.device)
         self.cov_prior_inv = torch.diag(1/(self.eta * torch.diag(cov_prior))).to(self.device)
         self.cov_semi = torch.diag(torch.sqrt(torch.diag(cov_prior))).to(self.device)
         self.cov_semi_inv = torch.diag(1/torch.sqrt(torch.diag(cov_prior))).to(self.device)
@@ -78,15 +77,6 @@ class VITS(object):
         eps = torch.normal(0, 1, size=(self.dimension,)).to(self.device)
         theta = self.mean + self.cov_semi @ eps
         return theta
-
-    # def sample_posterior(self):
-    #     eps = torch.normal(0, 1, size=(self.dimension,)).to(self.device)
-    #     theta = self.mean + self.cov_semi @ eps
-    #     if torch.isnan(theta).any():
-    #         print("NaN detected in sampled theta. Resetting theta to mean.")
-    #         theta = self.mean.clone()
-    #     return theta
-
         
     def reward(self, context):
         theta = self.sample_posterior()
@@ -119,31 +109,7 @@ class VITS(object):
                 hessian_matrices[idx, :, :] = hessian(self.potential, theta)
             del theta
         return gradients.mean(0), hessian_matrices.mean(0)
-
-    # def compute_gradient_hessian(self):
-    #     gradients = torch.zeros((self.mc_samples, self.dimension)).to(self.device)
-    #     hessian_matrices = torch.zeros((self.mc_samples, self.dimension, self.dimension)).to(self.device)
-    #     for idx in range(self.mc_samples):
-    #         theta = self.sample_posterior()
-    #         theta.requires_grad = True
-    #         current_grad = grad(self.potential(theta), theta)[0]
-    #         gradients[idx, :] = current_grad
-    #         if self.hessian_free:
-    #             theta.requires_grad = False
-    #             hessian_matrices[idx, :, :] = ((self.cov_semi_inv.T @ self.cov_semi_inv) @ (theta - self.mean)[:, None] @ current_grad[None, :])
-    #         else:
-    #             hessian_matrices[idx, :, :] = hessian(self.potential, theta)
-    #         del theta
-
-    #     # Check for NaNs
-    #     if torch.isnan(gradients).any() or torch.isnan(hessian_matrices).any():
-    #         print("NaN detected in gradients or hessian_matrices. Resetting to zeros.")
-    #         gradients = torch.zeros_like(gradients)
-    #         hessian_matrices = torch.zeros_like(hessian_matrices)
-
-    #     return gradients.mean(0), hessian_matrices.mean(0)
-
-    
+  
     def update_cov(self, h, hessian_matrix):
         cov_semi = (torch.eye(self.dimension).to(self.device) - h * hessian_matrix) @ self.cov_semi + h * self.cov_semi_inv.T
         if self.approx:
@@ -152,35 +118,8 @@ class VITS(object):
             cov_semi_inv = torch.linalg.pinv(cov_semi)
         return cov_semi, cov_semi_inv
 
-    # def update_cov(self, h, hessian_matrix):
-    #     # Handle invalid hessian_matrix
-    #     if torch.isnan(hessian_matrix).any() or torch.isinf(hessian_matrix).any():
-    #         print("Invalid hessian_matrix detected. Resetting to zeros.")
-    #         hessian_matrix = torch.zeros_like(hessian_matrix)
-
-    #     # Update cov_semi with regularization
-    #     cov_semi = (torch.eye(self.dimension).to(self.device) - h * hessian_matrix) @ self.cov_semi + h * self.cov_semi_inv.T
-    #     cov_semi += torch.eye(self.dimension).to(self.device) * 1e-6  # Regularization
-
-    #     # Compute inverse or fallback to identity
-    #     try:
-    #         cov_semi_inv = torch.linalg.pinv(cov_semi)
-    #     except RuntimeError as e:
-    #         print(f"Matrix inversion error: {e}. Resetting cov_semi_inv to identity.")
-    #         cov_semi_inv = torch.eye(self.dimension).to(self.device)
-
-    #     # Ensure no NaNs in outputs
-    #     assert not torch.isnan(cov_semi).any(), "cov_semi contains NaN"
-    #     assert not torch.isnan(cov_semi_inv).any(), "cov_semi_inv contains NaN"
-
-    #     return cov_semi, cov_semi_inv
-
-
-
-
     def update(self, context, action, reward):
         self.context = torch.cat([self.context, torch.tensor(context, dtype=torch.float32)[None, :].to(self.device)])
-        #self.context = torch.cat([self.context, context.clone().detach().to(self.device).unsqueeze(0)])
         self.rewards = torch.cat([self.rewards, torch.tensor([reward], dtype=torch.float32).to(self.device)])
         h = self.h / (len(self.rewards) * self.eta)
         for _ in range(self.nb_updates):
@@ -247,7 +186,7 @@ class Langevin(object):
 
 mac_hndlr = []
 
-global_ue_aggr_data = AggrData(max_length=5, datasets=['cqi', 'ul_demand', 'ul_throughput', 'power'])
+global_ue_aggr_data = AggrData(max_length=5, datasets=['cqi', 'ul_demand', 'ul_throughput', 'power', 'ul_snr', 'ul_bler', 'ul_prb', 'ul_mcs'])
 
 # Global lock to ensure thread-safe access to the global dictionary
 global_lock_data = threading.Lock()
@@ -334,17 +273,20 @@ class vran(object):
     def min_max_scale(self, value, min_val, max_val):
         return (value - min_val) / (max_val - min_val)
 
-    def scale_features(self, cqi, demand, tbs, power):
+    def scale_features(self, cqi, demand, tbs, power, snr):
         scaled_cqi = self.min_max_scale(cqi, 0, 15)  # Scale CQI to [0, 1]
-        scaled_demand = self.min_max_scale(demand, 0, 6000)  # Scale Demand to [0, 1]
+        scaled_demand = self.min_max_scale(demand, 0, 300000)  # Scale Demand to [0, 1]
         scaled_tbs = self.min_max_scale(tbs, 0, 6000)  # Scale TBS to [0, 1]
         scaled_power = self.min_max_scale(power, 0, 50)  # Scale Power to [0, 1]
-        return scaled_cqi, scaled_demand, scaled_tbs, scaled_power
+        scaled_snr = self.min_max_scale(snr, 0, 32)
+        return scaled_cqi, scaled_demand, scaled_tbs, scaled_power, scaled_snr
 
     
     def compute(self):
         cumulative_regret = torch.zeros((self.T,))
         average_regret = torch.zeros((self.T,))
+        mcs = torch.zeros((self.T,))
+        prb = torch.zeros((self.T,))
         context_cqi, context_demand = 0, 0
         observe_tbs, observe_pwr = 0, 0
         best_expected_reward = 0
@@ -352,18 +294,27 @@ class vran(object):
         start_time = time.time()
         for t in range(self.T):
             with global_lock_data:
+                # context
                 context_cqi = global_ue_aggr_data.get_stats('cqi')['mean']
                 context_demand = global_ue_aggr_data.get_stats('ul_demand')['mean']
+                context_snr = global_ue_aggr_data.get_stats('ul_snr')['mean']
+                # observation
                 observe_tbs = global_ue_aggr_data.get_stats('ul_throughput')['mean']
                 observe_pwr = global_ue_aggr_data.get_stats('power')['mean']
-                context_cqi, context_demand, observe_tbs, observe_pwr = self.scale_features(context_cqi, context_demand, observe_tbs, observe_pwr)
+                observe_bler = global_ue_aggr_data.get_stats('ul_bler')['mean']
+                observe_prb = global_ue_aggr_data.get_stats('ul_prb')['mean']
+                observe_mcs = global_ue_aggr_data.get_stats('ul_mcs')['mean']
+                context_cqi, context_demand, observe_tbs, observe_pwr, context_snr = self.scale_features(context_cqi, context_demand, observe_tbs, observe_pwr, context_snr)
             if t > 0:
                 reward = self.cal_reward((observe_tbs, context_demand, observe_pwr))
-                print(t, context_demand, action, reward, best_expected_reward)
+                print(t, context_snr, observe_bler, context_demand, reward, best_expected_reward)
                 self.agents[action].update(context, action, reward)
-                cumulative_regret[t] = cumulative_regret[t-1] + best_expected_reward - reward
-                average_regret[t] = cumulative_regret[t] / t
-            context = [context_cqi, context_demand]
+                average_regret[t] = best_expected_reward - reward
+                cumulative_regret[t] = cumulative_regret[t-1] + average_regret[t]
+                mcs[t] = observe_mcs
+                prb[t] = observe_prb
+                
+            context = [context_snr, context_demand]
             action, best_expected_reward = self.choose_action(context, self.agents)
             self.send_action(self.actions[action].cpu().numpy())
             #time.sleep(0.04)
@@ -371,8 +322,7 @@ class vran(object):
         end_time = time.time()
         print("Training time: {:.2f}".format(end_time - start_time))
         print("Stopping VITS")
-        return cumulative_regret
-        #return average_regret
+        return cumulative_regret, average_regret, mcs, prb
 
 
 class MACCallback(ric.mac_cb):
@@ -386,10 +336,14 @@ class MACCallback(ric.mac_cb):
             ue_stats = ind.ue_stats[0]
             with global_lock_data:
                 #if (self.ind_num > 100):
-                global_ue_aggr_data.add_data('cqi', 1, ind.tstamp) # ue_stats.wb_cqi
+                global_ue_aggr_data.add_data('cqi', ue_stats.wb_cqi, ind.tstamp) # ue_stats.wb_cqi
                 global_ue_aggr_data.add_data('ul_throughput', ue_stats.ul_curr_tbs, ind.tstamp)
                 global_ue_aggr_data.add_data('ul_demand', ue_stats.bsr, ind.tstamp)
                 global_ue_aggr_data.add_data('power', ue_stats.pwr, ind.tstamp)
+                global_ue_aggr_data.add_data('ul_snr', ue_stats.pusch_snr, ind.tstamp)
+                global_ue_aggr_data.add_data('ul_bler', ue_stats.ul_bler, ind.tstamp)
+                global_ue_aggr_data.add_data('ul_prb', ue_stats.ul_sched_rb, ind.tstamp)
+                global_ue_aggr_data.add_data('ul_mcs', ue_stats.ul_mcs1, ind.tstamp)
             #self.ind_num += 1
 
 def run_monitor(stop_event, conn, run_time_sec, frequency):
@@ -489,14 +443,38 @@ if __name__ == '__main__':
     # start resource allocation
     df = pd.DataFrame()
     for _ in range(args.rounds):
+        cum_regret, avg_regret, mcs, prb = env.compute()
         row = pd.DataFrame({'seed': args.seed,
                             'legend': f'{algo_name} - eta: {eta}',
                             'step': range(T),
-                            'cum_regret': env.compute()})
+                            'cum_regret': cum_regret,
+                            'avg_regret': avg_regret,
+                            'mcs': mcs,
+                            'prb': prb})
         df = pd.concat([df, row], ignore_index=True)
-        df.to_csv('/home/nakaolab/ra/vits_result.csv', index=False)
-    sns.lineplot(data=df, x='step', y='cum_regret', hue='legend')
-    plt.savefig('/home/nakaolab/ra/result.png')
+    
+    # Save the DataFrame to a CSV file
+    output_dir = '/home/nakaolab/ra'
+    os.makedirs(output_dir, exist_ok=True)  # Ensure the directory exists
+    result_csv_path = os.path.join(output_dir, 'vits_result.csv')
+    df.to_csv(result_csv_path, index=False)
+    print(f"Results saved to {result_csv_path}")
+
+    # Generate and save plots
+    plot_metrics = ['cum_regret', 'avg_regret', 'mcs', 'prb']
+    for metric in plot_metrics:
+        plt.figure(figsize=(10, 6))
+        sns.lineplot(data=df, x='step', y=metric, hue='legend')
+        plt.xlabel('Step', fontsize=14)
+        plt.ylabel(metric.replace('_', ' ').capitalize(), fontsize=14)
+        plt.title(f'{metric.replace("_", " ").capitalize()} over Steps', fontsize=16)
+        plt.legend(title='Legend', fontsize=12)
+        plt.grid(True)
+        plot_path = os.path.join(output_dir, f'{metric}.png')
+        plt.savefig(plot_path)
+        plt.close()
+        print(f"Plot saved to {plot_path}")
+    
     #plt.show()
     
 
